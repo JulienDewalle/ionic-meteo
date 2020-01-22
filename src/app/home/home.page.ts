3ae43d634459aa58528d771b3b5b51749817532f
Dewalle Julien <julien.dewalle@hotmail.fr>
2020-01-22 15:05:09 +0100

    add menu and page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation,
+    private router: Router
+    ) {}
 
+  ngOnInit() {
+    // Au chargement de l'appli on affiche les coordonnées GPS
+    this.geolocation.watchPosition().subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  // On se rend sur le page /about
+  navToAbout(){
+    this.router.navigate(['/about']);
+  }
 }
